d000e8d1fb4c63e0d076748afdbe663ca75d82e4
asrulsibaoel <asrulsibaoel@asrulsibaoel-PC>
2015-05-16 08:47:23 +0700

    Merge origin/master

--- common/config/timbang.php
+++ common/config/timbang.php
@@ -1,8 +1,8 @@
 <?php
 //=========change this scope=========================================================
-$root = '/var/www/timbang/';
+$root = '/var/www/html/asrul/sinarharapan/';
 //$root = 'D:/xampp/htdocs/taro_simpeg/'; //local
-$themesUrl = 'http://localhost/timbang/common/themes/';
+$themesUrl = 'http://localhost/asrul/sinarharapan/common/themes/';
 $db = 'landa_timbang';
 //$db = 'taro_simpeg_sampang_new'; //local
 $dbUser = 'root';
@@ -11,6 +11,6 @@ $dbPwd = 'landak';
 $client = 'timbang';
 $clientName = 'Sistem Informasi Pegawai Kab. Sampang';
 $menu = array();
-$rootUrl = 'http://localhost/timbang/timbang/www/'.$client.'/';
+$rootUrl = 'http://localhost/asrul/sinarharapan/timbang/www/'.$client.'/';
 //===================================================================================
 ?>
